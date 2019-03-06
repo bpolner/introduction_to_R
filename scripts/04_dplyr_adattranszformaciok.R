9acb798f18298ce1f0135454266f8400478a889d
Bertalan Polner <polner.b@gmail.com>
2019-03-06 11:37:26 +0100

    Adding data transformation and tidying + joining scripts

---
+++ scripts/04_dplyr_adattranszformaciok.R
@@ -0,0 +1,559 @@
+####  Transzformaciok az adatok felfedezese soran a dplyr csomag hasznalataval
+
+
+library(tidyverse)
+
+
+#### 0. Elokeszuletek es alapok ----------------------------------------------
+
+library(nycflights13)
+
+flights
+# valtozok nevei alatti roviditesek jelzik a valtozok tipusat
+# int - integer = egesz szam
+# dbl - double = valos szam
+# chr - character = betulancok (strings)
+# dttm - date-time = datum es ido
+# lgl - logical = logikai
+# fctr - factor = kategorikus valtozok fix lehetseges ertekekkel
+# date - date = datum
+
+# A dplyr szintaxisanak alapjai:
+
+# Megfigyelesek kivalasztasa ertekeik alapjan: filter().
+# Sorok ujrarendezese: arrange().
+# Valtozok kivalasztasa nev szerint: select().
+# Letezo valtozok alapjan uj valtozok szamolasa: mutate().
+# Egy osszefoglalo szamolasa sok ertekbol: summarise().
+
+# Mûveletek csoportonként: group_by().
+
+# Mûködésük: 
+# Elsõ argumentum mindig az adat (data.frame)
+# Következõ argumentumok: mit tegyünk az adattal, melyik változókkal (idézõjel nélkül)
+# Eredményül mindig egy adatot (data.frame) kapunk vissza
+
+
+#### 1. Sorok szurese: filter()  --------------------------------
+
+# 1. arg.: adat, következõ arg.(ok): szûrési feltételek kifejezése
+
+# Jaratok jan. elsejen:
+
+filter(flights, month == 1, day == 1)
+
+# A dplyr függvények sosem módosítják a bemenetet. 
+# Az eredmény tárolásához hozzá kell rendeljük egy változóhoz: 
+
+jan1 <- filter(flights, month == 1, day == 1)
+
+# Egyszerre taroljuk, es nezzuk is meg a szûrés eredményét
+
+(dec25 <- filter(flights, month == 12, day == 25))
+
+# Osszehasonlitasok logikai operatorokkal:
+# < , > , <= , >= , != , ==
+
+# Vigyazat! Logikai vizsgálathoz kettõ egyenlõségjel kell!
+
+filter(flights, month = 1)
+
+# Anomáliák a == használata során:
+
+sqrt(2)^2 == 2
+1/49 * 49 == 1
+
+# Az oka a szamitogep veges pontossaga. 
+# A problema megkerulheto a near() hasznalataval.
+
+near(sqrt(2)^2, 2)
+near(1 / 49 * 49, 1)
+
+# Szûrési feltételek kombinálása logikai operátorokkal:
+# http://r4ds.had.co.nz/diagrams/transform-logical.png 
+# & , | , ! , xor
+
+filter(flights, month == 11 | month == 12 | month == 3 | month == 1)
+
+# Tobb lehetseges ertek felsorolasa roviden (a fentivel ekvivalens)
+# (month értéke eleme a vektorral megadott halmaznak)
+
+nov_dec <- filter(flights, month %in% c(11, 12, 3, 1))
+
+
+# A hiányzó értékek (NA) megkeseríthetik az életünket, 
+# ha összehasonlításokat végzünk
+
+NA > 5
+10 == NA
+NA + 10
+NA / 2
+
+# Ami igazán zavarbaejtõ:
+
+NA == NA
+
+  
+# Kontextusba helyezve könnyebben érthetõ:
+# Legyen x Anna életkora, ami nem ismert
+
+x <- NA
+
+# Legyen y Béla életkora, ami szintén nem ismert
+
+y <- NA
+
+# Egyidõs Anna és Béla?
+
+x == y
+
+# Nem tudhatjuk!
+
+
+# Hiányzik-e az adott érték: is.na()
+
+is.na(x)
+
+
+# 1.1 Gyakorlas - filter() ------------------------------------------------
+
+# 1) Valasszuk ki az osszes jaratot, ami
+# a, legalabb 2 ora kesessel erkezetett
+
+
+# b, nyaron repult (junius, julius, augusztus)
+
+
+# c, tobb, mint ket orat kesett, pedig pontosan indult
+
+
+# 2) Mire jo a between() fuggveny? 
+
+
+# 3) Hany jaratnak hianyzik az indulasi ideje (dep_time)?
+
+
+
+#### 2. Sorok sorbarendezese: arrange() ------------------------
+
+arrange(flights, year, month, day)
+
+
+# Csokkeno sorrend:
+
+arrange(flights, desc(arr_delay))
+
+# Hianyzo ertekek mindig hatulra kerulnek:
+
+df <- tibble(x = c(5, 2, NA))
+df
+arrange(df, x)
+arrange(df, desc(x))
+
+
+
+# 2.1 Gyakorlas - arrange() -----------------------------------------------
+
+# 1) Melyik jaratok kestek a legtobbet? Es melyek indultak el leghamarabb?
+
+
+# 2) Melyik jaratok repultek a legmesszebbre? Es a legkisebb tavolsagra?
+
+
+
+#### 3. Valtozok kivalasztasa: select() --------------------------------------
+
+select(flights, year, month, day)
+
+# Ket valtozo, es a koztuk levok is
+
+select(flights, year:day)
+
+# Valtozok kihajitasa
+
+select(flights, -(year:day))
+
+# Hasznos segedfuggvenyek 
+# http://dplyr.tidyverse.org/reference/select_helpers.html 
+
+select(flights, starts_with("dep"))
+select(flights, ends_with("time"))
+select(flights, contains("arr_") )
+
+
+# Valtozok atnevezesehez: rename()
+
+rename(flights, tail_num = tailnum)
+
+# Ha nehany valtozot az adattábla elejere akarunk tenni, az összes 
+# többit meg csak egyben megfogni: everything()
+
+select(flights, time_hour, air_time, everything())
+
+
+
+# 3.1 Gyakorlás: select() -------------------------------------------------
+
+# 1) Tedd a flights adattábla azon oszlopait, melyekben késéssel 
+# kapcsolatos információ van! Utána következzenek azok az oszlopok, 
+# amelyek nevében szerepel a "time"! A táblából vedd ki a légitársaság, a 
+# járat, és a gép azonosítóit, illetve az induló- és a célállomást!
+
+
+
+#### 4. Uj valtozok szamolasa: mutate() --------------------------------------
+
+# Válasszunk ki néhány oszlopot, hogy a konzolon is jól lássuk, 
+# mi történik!
+
+flights_sml <- 
+  select(
+    flights, 
+    year:day, 
+    ends_with("delay"), 
+    distance, 
+    air_time
+  )
+
+
+# Uj valtozok szamolasa
+
+mutate(
+  flights_sml,
+  gain = arr_delay - dep_delay,
+  speed = distance / air_time * 60
+)
+
+
+# Egy mutate-n belül utalhatunk akar az eppen letrehozott valtozokra is!
+
+mutate(
+  flights_sml,
+  gain = arr_delay - dep_delay,
+  hours = air_time / 60,
+  gain_per_hour = gain / hours
+)
+
+
+# transmute: ha csak a létrehozott változókat akarod visszakapni:
+
+transmute(
+  flights,
+  gain = arr_delay - dep_delay,
+  hours = air_time / 60,
+  gain_per_hour = gain / hours
+)
+
+
+# mutate()-n belül bármilyen vektorizált függvény használható
+# (azaz bemenete vektor és egyezõ hosszúságú vektort ad vissza)
+# Bõvebben: http://r4ds.had.co.nz/transform.html#select-columns-with-select 5.5.1
+
+# 4.1 Gyakorlás: mutate() -------------------------------------------------
+
+# Számold ki újra az indulási és érkezési késést!
+
+
+
+#### 5. Összefoglalások: summarise() ----------------------------
+
+# A summarise() bemenete egy adattábla (data.frame), és visszaad egyetlen sort!
+
+summarise(
+  flights, 
+  delay = mean(dep_delay, na.rm = TRUE)
+)
+
+# Akkor igazán hasznos, ha a group_by()-al egyutt hasznaljuk
+
+by_day <- group_by(flights, year, month, day)
+summarise(by_day, delay = mean(dep_delay, na.rm = TRUE))
+
+# Mit csinalt a group_by() ?
+
+class(flights)
+class(by_day)
+
+
+## 5.1 Tobb muvelet osszekotese a pipe (%>%) hasznalataval  -------------------
+
+
+by_dest <- group_by(flights, dest)
+
+delay <- 
+  summarise(
+    by_dest,
+    count = n(),
+    dist = mean(distance, na.rm = TRUE),
+    delay = mean(arr_delay, na.rm = TRUE)
+  )
+
+delay <- filter(delay, count > 20, dest != "HNL")
+
+ggplot(data = delay, mapping = aes(x = dist, y = delay)) +
+  geom_point(aes(size = count), alpha = 1/3) +
+  geom_smooth(se = FALSE)
+
+# Ez most 4 lepes volt: adatok csoportositasa, osszegzes, szures, majd ábrázolás.
+# A koztes adattáblák foloslegesek, jo lenne kihagyni oket.
+
+flights %>% 
+  group_by(dest) %>% 
+  summarise(
+    count = n(),
+    dist  = mean(distance, na.rm = TRUE),
+    delay = mean(arr_delay, na.rm = TRUE)
+  ) %>% 
+  filter(count > 20, dest != "HNL") %>% 
+  ggplot(mapping = aes(x = dist, y = delay)) +
+  geom_point(aes(size = count), alpha = 1/3) +
+  geom_smooth(se = FALSE)
+
+
+# %>% beszurasahoz Ctrl + Shift + M 
+# Mit csinal a %>% ?
+# x %>% f(y) = f(x, y)
+# Olvashatjuk úgy is, hogy %>% = "aztán": 
+# csoportosítsd, AZTÁN összegezd, AZTÁN szûrd, AZTÁN ábrázold
+
+# Hianyzo adatok szurese
+
+# Mire volt jó fentebb az na.rm = TRUE?
+
+flights %>% 
+  group_by(year, month, day) %>% 
+  summarise(mean = mean(dep_delay))
+
+# Az aggregációs függvényekre is érvényes:
+# ha hiányos adatsort kapnak, NA-t adnak vissza
+
+# A flights adattáblában a hianyzo keses a lemondott jaratoknal szerepel
+# Egy megoldás lehet, ha kihajítjuk ezeket a sorokat:
+  
+not_cancelled <- flights %>% 
+  filter(!is.na(dep_delay), !is.na(arr_delay))
+
+# Mi tortenik itt?
+
+not_cancelled %>% 
+  group_by(year, month) %>% 
+  summarise(mean = mean(dep_delay))
+
+# Nezzuk meg gepenkent (tailnum) az atl. indulasi kesest!
+
+delays <- not_cancelled %>% 
+  group_by(tailnum) %>% 
+  summarise(
+    delay = mean(arr_delay)
+  )
+
+ggplot(data = delays, mapping = aes(x = delay)) + 
+  geom_freqpoly(binwidth = 10)
+
+# Van olyan gep, ahol az ATLAG indulasi keses ~5 ora!
+
+# Aggregációknál érdemes megnézni, hány megfigyelést összegeztünk.
+# Hany ut alapjan lett az atlag keses megallapitva?
+
+delays <- not_cancelled %>% 
+  group_by(tailnum) %>% 
+  summarise(
+    delay = mean(arr_delay, na.rm = TRUE),
+    n = n()
+  )
+
+ggplot(data = delays, mapping = aes(x = n, y = delay)) + 
+  geom_point(alpha = 1/10)
+
+# Jellemzõ mintázat: mintanagyság növekedésével csökken az összegzõ statisztika szóródása
+# Aggregalasnal mindig erdemes rogziteni az esetek szamat: 
+# n() vagy sum(!is.na())
+
+# Szûrjük a kevés megfigyelésen alapuló átlagokat, és aztán ábrázoljunk
+
+delays %>% 
+  filter(n > 25) %>% 
+  ggplot(mapping = aes(x = n, y = delay)) + 
+  geom_point(alpha = 1/10)
+
+# Aggregalas es logikai indexeles egyutt:
+
+not_cancelled %>% 
+  group_by(year, month, day) %>% 
+  summarise(
+    avg_delay1 = mean(arr_delay),
+    avg_delay2 = mean(arr_delay[arr_delay > 0]) # az átlagos pozitív késés
+  )
+
+
+## 5.2 Leiro statisztikak keszitesenel hasznos fuggvenyek. -----------------
+
+# Szorodas
+
+?sd
+?IQR
+
+not_cancelled %>% 
+  group_by(dest) %>% 
+  summarise(distance_sd = sd(distance)) %>% 
+  arrange(desc(distance_sd))
+
+# Rang
+
+?min
+quantile(1:6, probs = 0.33)
+?max
+
+not_cancelled %>% 
+  group_by(year, month, day) %>% 
+  summarise(
+    first = min(dep_time),
+    last = max(dep_time)
+  )
+
+# Darabszam
+
+?n # darabszám
+?n_distinct # egyedi esetek
+
+not_cancelled %>% 
+  group_by(dest) %>% 
+  summarise(carriers = n_distinct(carrier)) %>% 
+  arrange(desc(carriers))
+
+# Darabszam gyorsan - hany jarat ment az egyes celallomasokra?
+
+not_cancelled %>% 
+  count(dest)
+
+# Sulyozassal - mekkora tavolsagot repultek az egyes celallomasokra osszesen?
+
+not_cancelled %>% 
+  count(dest, wt = distance)
+
+# Feltetelnek megfelelo esetek mennyisege 
+
+not_cancelled %>% 
+  group_by(year, month, day) %>% 
+  summarise(n_early = sum(dep_time < 500))
+
+# Csoportositas megszuntetese
+
+daily %>% 
+  ungroup() %>%             # no longer grouped by date
+  summarise(flights = n())  # all flights
+
+
+# 5.3 Gyakorlás: summarise() ----------------------------------------------
+
+
+# 1) Hozz létre a lentiek eredményét a count() használata nélkül!
+
+not_cancelled %>% 
+  count(dest)
+
+
+not_cancelled %>%
+  count(tailnum, wt = distance)
+
+
+# 2) A törölt járatok meghatározása (is.na(dep_delay) | is.na(arr_delay) ) 
+# nem a legoptimálisabb. Miért? Melyik oszlop a legfontosabb?
+
+
+
+# 3) Nézd meg a hónap napjai szerinti bontásban a lemondott járatok számát.
+# Látsz-e valamilyen mintázatot? 
+# A lemondott járatok aránya összefügg-e az átlagos késéssel?
+
+
+
+# 4) Melyik légitársaság (carrier) késik a legtöbbet? 
+# El tudjuk-e különíteni a rossz repterek vs. a rossz légitársaságok
+# hatását? Miért / miért nem?
+# (Tipp: flights %>% group_by(carrier, dest) %>% summarise(n()))
+
+
+
+# 6. Haladó dplyr ------------------------------------------------------------
+
+
+# 6.1 Több oszlop transzformálása röviden ----
+
+# http://dplyr.tidyverse.org/reference/summarise_all.html 
+
+
+# 1) Az összes oszlopra:
+
+iris %>% 
+  group_by(Species) %>% 
+  summarise_all(median)
+
+# Több összefoglalás egyszerre:
+
+# a) függvények felsorolása a funs()-on belül
+
+iris %>% 
+  group_by(Species) %>% 
+  summarise_all(funs(median, mean))
+
+# b) függvények nevének megadása karaktervektorban
+
+iris %>% 
+  group_by(Species) %>% 
+  summarise_all(c("median", "mean"))
+
+
+# 2) Megadott feltételtõl függõen:
+
+# Átlag számítása az összes valós számot tartalmazó oszlopra
+
+flights %>% 
+  summarise_if(is.double, mean, na.rm = TRUE)
+
+# Karaktereket tartalmazó változók faktorrá alakítása
+
+flights %>% 
+  mutate_if(is.character, as.factor)
+
+# 3) Oszlopok neve alapján:
+
+# a) select()-ben használt módszerrel a vars()-on belül (segédfüggvények is mûködnek!)
+
+flights %>%
+  summarise_at(vars(dep_time:arr_time), max, na.rm = T)
+
+flights %>% 
+  summarise_at(vars(ends_with("delay")), mean, na.rm = TRUE)
+
+# b) karaktervektorral megadva
+
+flights %>% 
+  summarise_at(c("arr_delay", "dep_delay"), mean, na.rm = TRUE)
+
+
+# 6.2 Mûveletek es szûrõk csoportosítással ----
+
+# Minden napra a 3 legtöbbet késõ járat kikeresése:
+
+flights_sml %>% 
+  group_by(year, month, day) %>%
+  filter(rank(desc(arr_delay)) < 4)
+
+
+# Kuszobnel nagyobb csoportok meghatarozasa
+# Pl. csak a népszerû célállomások:
+
+popular_dests <- 
+  flights %>% 
+  group_by(dest) %>% 
+  filter(n() > 3000)
+
+popular_dests
+
+# Standardizalas es csoportokon beluli mutatok szamolasa
+
+popular_dests %>% 
+  filter(arr_delay > 0) %>% 
+  mutate(prop_delay = arr_delay / sum(arr_delay)) %>% 
+  select(year:day, dest, arr_delay, prop_delay) 
---
+++ scripts/05_tidy_data_join.R
@@ -0,0 +1,524 @@
+# Data wrangling
+
+library(tidyverse)
+
+
+# 1. Adatrendezes (tidy data) ---------------------------------------------
+
+# Ugyanaz az adat tobbfelekeppen is tárolható
+
+table1
+table2
+table3
+table4a; table4b
+
+# Mitol lesz rendezett (tidy) az adat?
+# 1. Minden valtozo kulon oszlop
+# 2. Minden megfigyeles kulon sor
+# 3. Minden ertek kulon cella
+
+# A fenti 4 pelda kozul melyik tekintheto rendezettnek?
+
+# Konzisztensen rendezett adatok segitik altalanos elvek alkalmazasat,
+# amikor az adatokkal dolgozunk
+# tidyverse csomagjai feltetelezik az adatok rendezettseget
+
+# Pl. 10 000 fore eso gyakorisagi arany
+table1 %>% 
+    mutate(rate = cases / population * 10000)
+
+# Vagy az evenkenti esetek szama
+table1 %>% 
+    count(year, wt = cases)
+
+# Valtozas egy ev alatt
+ggplot(table1, aes(year, cases)) + 
+    geom_line(aes(group = country), colour = "grey50") + 
+    geom_point(aes(colour = country))
+
+# Hogyan készítenétek el ugyanezeket a kimutatásokat és
+# az ábrát table2, ill. table4a és table4b alapján?
+
+
+
+# 1.1 Szetterites es osszegyujtes -------------------------------------------
+
+# Az adatok sajnos a legtöbbször nincsenek tidy formátumba rendezve.
+# Miért?
+# Sokan nem ismerik, és nem evidens
+# Az adatok rendezése sokszor nem az elemzést, hanem a bevitelt segíti
+
+# 1.: Mik a változók és a megfigyelések?
+
+# 2.: Két problémát kell megoldani:
+# - egy változóhoz tartozó adatok több oszlopban lehetnek
+# - egy megfigyelés több sorba is szét lehet szóródva
+# Általában egyszerre csak az egyik áll fenn.
+
+# Ezeket a problémákat orvosolja a tidyr két alapvetõ függvénye:
+# gather() és spread()
+  
+# gather (=összegyûjtés): ha az oszlopnevek egy változó értékeit veszik fel
+
+table4a
+
+# Három fontos paraméter:
+
+# - Melyek az oszlopok, amikben nem változók, hanem értékek vannak?
+# - Mi az a változó, aminek az értékeit a változónevek ebben a táblában? (key)
+# - Mi az a változó, aminek az értékei a cellákban vannak? (value)
+
+table4a %>% 
+    gather(`1999`, `2000`, key = "year", value = "cases")
+
+# Az oszlopokat úgy határozzuk meg, mint a dplyr::select()-nél.
+
+
+# Hasonloan eljarhatunk a table4b eseteben is, csak itt ugye
+# egy másik változó értékei vannak a cellákban, módosítsuk a value paramétert!
+
+table4b %>% 
+    gather(`1999`, `2000`, key = "year", value = "population")
+
+# Ahhoz, hogy egy tibble-be kerüljenek az adataink, hasznaljuk a left_join()-t
+
+tidy4a <- table4a %>% 
+    gather(`1999`, `2000`, key = "year", value = "cases")
+tidy4b <- table4b %>% 
+    gather(`1999`, `2000`, key = "year", value = "population")
+
+left_join(tidy4a, tidy4b)
+
+# Kicsit késõbb lesz szó a join függvényekrõl is!
+
+# spread (=szétterítés): ha van egy oszlop, 
+# amiben változók nevei szerepelnek
+
+table2
+
+# Most kettõ fontos paraméter:
+# - Melyik az az oszlop, amelyik a változók neveit tartalmazza? (key)
+# - Melyik az az oszlop, ami több változó értékeit is tartalmazza? (value)
+
+spread(table2, key = type, value = count)
+
+# gather: szélesbõl hosszúba (wide to long)
+# spread: hosszúból szélesbe (long to wide)
+
+
+# 1.1.1 Gyakorlas - spread es gather --------------------------------------
+
+# 1) Rendezd a preg adatokat tidy formátumba!
+
+preg <- tribble(
+    ~pregnant, ~male, ~female,
+    "yes",     NA,    10,
+    "no",      20,    12
+)
+
+preg %>% 
+  gather(male, female, key = "nem", value = "darabszam")
+
+
+# 1.2 Elválasztás es egyesítés --------------------------------------------
+
+# Elõfordulhat az is, hogy egy oszlopban több változó értéke is szerepel!
+
+table3
+
+# Ilyenkor jön jól a separate()
+
+table3 %>% 
+    separate(rate, into = c("cases", "population"))
+
+# A separate() elvalasztokent azonosítja a nem-alfanumerikus karaktereket.
+# Az elválasztót meg is adhatjuk:
+
+table3 %>% 
+    separate(rate, into = c("cases", "population"), sep = "/")
+
+# Hoppa! A szamok karakterkent kepezodtek le 
+# - mert ugye karakterkent voltak a forrasban, és alapesetben
+# a separate() nem változtatja meg az oszlopok típusát
+# convert=TRUE: megprobalja jobb adattipusra alakitani 
+# az oszlopot az elvalasztas utan
+
+table3 %>% 
+    separate(rate, into = c("cases", "population"), convert = TRUE)
+
+# sep argumentumnak egész számokkal azt is megadhatjuk, hogy
+# hány karakterenként vágjon:
+
+table3 %>% 
+    separate(year, into = c("century", "year"), sep = 2)
+
+# Ez itt most nem túl hasznos, de van, amikor jól jöhet!
+
+# unite() (=egyesítés): több oszlopból egy értéket
+
+table5
+
+table5 %>% 
+  unite(new, century, year)
+
+# Alapértelmezett szeparátor: _ 
+# Ha nem szeretnénk szeparátort:
+
+table5 %>% 
+    unite(new, century, year, sep="")
+
+
+
+# 1.2.1 Gyakorlás - elválasztás és egyesítés ------------------------------
+
+# 1) Mit tud a separate() extra és fill argumentuma?
+
+tibble(x = c("a,b,c", "d,e,f,g", "h,i,j")) %>% 
+  separate(x, c("one", "two", "three"))
+
+
+tibble(x = c("a,b,c", "d,e", "f,g,i")) %>% 
+  separate(x, c("one", "two", "three"))
+
+
+
+# 1.3 Hiányzó adatok  -----------------------------------------------------
+
+# Hiányzó adatok kétféleképpen lehetnek jelen az adattáblában:
+# - Explicit: NA-val jelölve (a hiány jelenléte)
+# - Implicit: egyszerûen csak nincs meg az adat (a jelenlét hiánya)
+
+# A lenti peldaban hol vannak ilyenek?
+
+stocks <- tibble(
+    year   = c(2015, 2015, 2015, 2015, 2016, 2016, 2016),
+    qtr    = c(   1,    2,    3,    4,    2,    3,    4),
+    return = c(1.88, 0.59, 0.35,   NA, 0.92, 0.17, 2.66)
+)
+
+# Masfajta reprezentacio explicitte teheti az osszes hianyzo adatot
+
+stocks %>% 
+    spread(year, return)
+
+# Ha a hianyzo adatok explicit megjelenitese nem erdekes, implicitte tehetjuk
+# gather(na.rm = TRUE)
+
+stocks %>% 
+    spread(year, return) %>% 
+    gather(year, return, `2015`:`2016`, na.rm = TRUE)
+
+stocks %>% 
+  spread(year, return) %>% 
+  gather(year, return, `2015`:`2016`)
+
+
+# Az implicit hiányzó adatokat explicitté tehetjük a complete()-tal is:
+
+stocks %>% 
+    complete(year, qtr)
+
+# Meg egy fontos dolog: 
+# hianyzo adatok "macskakormozest" is jelenthetnek 
+# (adatbeviteli gyakorlat)
+
+treatment <- tribble(
+    ~ person,           ~ treatment, ~response,
+    "Derrick Whitmore", 1,           7,
+    NA,                 2,           10,
+    NA,                 3,           9,
+    "Katherine Burke",  1,           4
+)
+
+# Ilyenkor használjuk a fill()-t!
+
+treatment %>% 
+    fill(person)
+
+# Fentrõl lefelé haladva a megadott oszlopban a hiányzó értékeket
+# a legutolsó nem hiányzó értékkel helyettesíti be
+# ("last observation carried forward" módszer)
+
+# fill() .direction argumentuma:
+
+treatment %>% 
+  fill(person, .direction = "up")
+
+
+# Még a tidy formátumról: www.jstatsoft.org/v59/i10/paper 
+# A nem-tidy formátumokról: https://simplystatistics.org/2016/02/17/non-tidy-data/ 
+
+
+# 2. Adattablak kapcsolasa ------------------------------------------------
+
+require(nycflights13)
+
+# Az adataink gyakran több adattáblában vannak.
+# Pl. jegyzõkonyv + kísérleti eredmények + kérdõívek. 
+
+# nycflights13 csomagban több tibble is van:
+airlines
+airports
+planes
+weather
+
+# Hogyan kapcsolódnak egymáshoz ezek a táblák?
+# http://r4ds.had.co.nz/diagrams/relational-nycflights.png
+
+# Az adattablakat "kulcsok" (keys) segitsegevel köthetjük össze
+# Kulcs: olyan változó (vagy változóhalmaz), 
+# ami egy megfigyelés azonosítására alkalmas
+# 
+# Elsodleges (primary) kulcsok: 
+#   egyertelmuen azonositanak egy megfigyelest adott tablan belul
+#   pl. planes$tailnum
+# Idegen (foreign) kulcsok: 
+#   egyertelmuen azonositanak egy megfigyelest egy masik tablan 
+#   pl. flights$tailnum 
+# 
+# Egy valtozo lehet egyszerre mindketto is!
+# 
+# Egy elsõdleges kulcs és egy másik tábla idegen kulcsa 
+# együttesen alkotnak egy kapcsolatot
+
+# Ha azonositottunk egy elsodleges kulcsot, 
+# erdemes ellenorizni, hogy tenyleg egyedi-e.
+# Nem tartozik-e veletlenul tobb megfigyeleshez is?
+
+planes %>% 
+    count(tailnum) %>% 
+    filter(n > 1)
+
+weather %>% 
+    count(year, month, day, hour, origin) %>% 
+    filter(n > 1)
+
+# Elõfordulhat, hogy egy táblában nincs elsõdleges kulcs!
+# A járatszám / gép sorszáma + dátum elsõdleges kulcs a flights táblában? 
+
+flights %>% 
+  count(year, month, day, flight) %>% 
+  filter(n > 1)
+
+flights %>% 
+  count(year, month, day, tailnum) %>% 
+  filter(n > 1)
+
+# Ilyenkor célszerû létrehozni minden megfigyelésnek egy sorszámot 
+# (mesterséges/helyettesítõ kulcs  = surrogate key)
+
+stocks %>% 
+  mutate(
+    rownumm = row_number()
+  )
+
+
+# 2.1 Mutating join -------------------------------------------------------
+
+# Egyezteti a megfigyeleseket a kulcsok alapjan, aztan 
+# egyik táblából a másikba masol valtozokat
+
+# Hasznaljunk egy keskenyebb tablat, 
+# hogy jol lassuk a kapcsolasok eredmenyet:
+
+flights2 <- flights %>% 
+    select(year:day, hour, origin, dest, tailnum, carrier)
+
+flights2
+
+# Pl. légitársaság teljes nevét (airlines tábla) 
+# adjuk hozzá a flights2 táblához:
+
+flights2 %>%
+    select(-origin, -dest) %>% 
+    left_join(airlines, by = "carrier")
+
+# Ugyanezt megoldhattuk volna a mutate használatával is,
+# igaz, kevésbé átlátható, és több változóval sokkal bonyolultabb lenne
+
+flights2 %>%
+  select(-origin, -dest) %>% 
+  mutate(name = airlines$name[match(carrier, airlines$carrier)])
+
+# A kapcsolások alapjait ezeken az egyszerû táblákon fogjuk szemlélteni:
+
+x <- tribble(
+    ~key, ~val_x,
+    1, "x1",
+    2, "x2",
+    3, "x3"
+)
+
+y <- tribble(
+    ~key, ~val_y,
+    1, "y1",
+    2, "y2",
+    4, "y3"
+)
+
+z <- tribble(
+    ~key, ~val_z,
+    1, "z1",
+    2, "z2",
+    5, "z3"
+)
+
+# Inner join
+# Csak a mindkét táblában meglévõ megfigyeléseket tartja meg
+
+x %>% 
+    inner_join(y, by = "key")
+
+# Outer join
+# Akkor tart meg egy megfigyelést, 
+# ha az legalább az egyik táblában elõfordult
+# http://r4ds.had.co.nz/diagrams/join-outer.png 
+
+# left_join(): az összes bal oldali táblában meglévõ megfigyelést megtartja
+
+x %>% 
+    left_join(y,  by = "key") 
+
+# right_join(): a jobb oldali táblából tartja meg az összes megfigyelést
+
+x %>% 
+    right_join(y, by = "key") 
+
+# full_join(): mindkét tábla összes megfigyelését megtartja
+
+x %>% 
+    full_join(y, by = "key")
+
+# join-ok Venn-diagramon: http://r4ds.had.co.nz/diagrams/join-venn.png 
+
+# Mi történik, ha a kulcsok nem azonosítanak egyértelmûen egy megfigyelést?
+
+# A) Egyik tablaban duplikalt, masikban egyedi
+# pl. ha egy külön táblában tárolunk extra információt
+# egy-kapcsolódik-sokhoz (pl. járatok-repterek)
+
+x <- tribble(
+    ~key, ~val_x,
+    1, "x1",
+    2, "x2",
+    2, "x3",
+    1, "x4"
+)
+y <- tribble(
+    ~key, ~val_y,
+    1, "y1",
+    2, "y2"
+)
+
+left_join(x, y, by = "key")
+
+# B) Mindket tablaban duplan van egy kulcs: 
+# ez altalaban hiba, mert a kulcs egyik táblában sem elsõdleges.
+# Ha ezeket egyesitjuk, az osszes lehetseges kombinaciot megkapjuk
+
+x <- tribble(
+    ~key, ~val_x,
+    1, "x1",
+    2, "x2",
+    2, "x3",
+    3, "x4"
+)
+y <- tribble(
+    ~key, ~val_y,
+    1, "y1",
+    2, "y2",
+    2, "y3",
+    3, "y4"
+)
+
+left_join(x, y, by = "key")
+
+# Kulcsok meghatarozasa *_join() fuggvenyek by argumentuma
+
+# A) Alapbeallitas: by=NULL. Minden kozos valtozot felhasznal (natural join)
+
+flights2 %>% 
+    left_join(weather)
+
+# B) Karaktervektorral meghatarozva
+# Pl year valtozo van mindket tibble-ben, de nem ugyanazt jelenti!
+
+flights2 %>% 
+    left_join(planes, by = "tailnum")
+
+# C) Nevesitett karaktervektorral pl. c("a" = "b")
+
+# Célállomás koordinátáit kapcsoljuk:
+
+flights2 %>% 
+    left_join(airports, c("dest" = "faa"))
+
+# Kiinduló állomás koordinátáit kapcsoljuk:
+
+flights2 %>% 
+  left_join(airports, c("origin" = "faa"))
+
+
+# 2.1.1 Gyakorlás - mutating join -----------------------------------------
+
+# 1) Van-e összefüggés egy repülõgép életkora és a késései között?
+
+flights %>%
+  # Mennyit szokott késni egy gép?
+  group_by(tailnum) %>%  
+  summarise(
+    avg_arr_delay = mean(arr_delay, na.rm = TRUE)
+  ) %>% 
+  # Táblák kapcsolása - mi a kulcs?
+  left_join(planes, key = "tailnum") %>% 
+  # Számoljuk ki, hogy hány éves a gép?
+  mutate(
+    plane_age = 2013-year
+  ) %>% 
+  # Válasszuk ki csak a fontos változókat!
+  select(tailnum, avg_arr_delay, plane_age) %>% 
+  # Készítsünk egy pontfelhõt trendvonallal!
+  ggplot(aes(x = plane_age, y = avg_arr_delay)) + 
+  geom_point(alpha = 0.07) + 
+  geom_smooth()
+
+  
+
+# 2.2 Filtering join ------------------------------------------------------
+
+# A logika hasonló, mint a mutating join-nak,
+# de csak a megfigyelésekre van hatással
+
+# semi_join(x, y) - megtart minden megfigyelést x-bõl, aminek van párja y-ban
+
+# anti_join(x, y) - kidob minden megfigyelést x-bõl, aminek van párja y-ban
+
+# Melyik a tíz legfrekventáltabb reptér?
+
+top_dest <- flights %>%
+  count(dest, sort = TRUE) %>%
+  head(10)
+top_dest
+
+# Melyik járatok mentek ezekre a repterekre?
+
+# Használhatnánk egy filtert:
+
+flights %>% 
+  filter(dest %in% top_dest$dest)
+
+# Több változóval nehezebb lenne 
+# (pl. megkeresni a top10 napot napi átlag késés mentén),
+# ezért jobb a semi_join:
+
+flights %>% 
+  semi_join(top_dest)
+
+# az anti_join pedig hasznos lehet, 
+# ha a kapcsolás során az egyezés hiányát akarjuk megnézni
+
+# Melyik járatoknak nincs párja a planes táblában?
+
+flights %>%
+  anti_join(planes, by = "tailnum") %>%
+  count(tailnum, sort = TRUE)
