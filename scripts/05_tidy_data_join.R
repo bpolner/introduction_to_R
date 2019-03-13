922b096d5c15700c088d7c39500eeb9ebf42833c
Bertalan Polner <polner.b@gmail.com>
2019-03-13 10:43:59 +0100

    Added another exercise to gather & spread

--- scripts/05_tidy_data_join.R
+++ scripts/05_tidy_data_join.R
@@ -119,6 +119,18 @@ preg <- tribble(
 )
 
 
+# 2) Miért nem tökéletesen szimmetrikus a gather és a spread? 
+
+stocks <- tibble(
+  year   = c(2015, 2015, 2016, 2016),
+  half  = c(   1,    2,     1,    2),
+  return = c(1.88, 0.59, 0.92, 0.17)
+)
+
+stocks %>% 
+  spread(year, return) %>% 
+  gather("year", "return", `2015`:`2016`)
+
 
 # 1.2 Elválasztás es egyesítés --------------------------------------------
 
